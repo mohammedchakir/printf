81787315990e304590297d78c9d7190d2e311da2
mohammedchakir <zerroukimedchakir@gmail.com>
2023-08-21 17:33:58 -0700

    degugging

--- 6_get_precision.c
+++ 6_get_precision.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * get_precision - Calculates the precision
+ * handle_precision - Calculates the precision
  * @format: Formatted string
  * @i: List of arguments to print
  * @arg_list: list of arguments.
  * Return: Precision.
  */
-int get_precision(const char *format, int *i, va_list list)
+int handle_precision(const char *format, int *i, va_list arg_list)
 {
 	int i_printed = *i + 1;
 	int prec = -1;
@@ -27,7 +27,7 @@ int get_precision(const char *format, int *i, va_list list)
 		else if (format[i_printed] == '*')
 		{
 			i_printed++;
-			prec = va_arg(list, int);
+			prec = va_arg(arg_list, int);
 			break;
 		}
 		else
