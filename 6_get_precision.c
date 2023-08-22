7e29ac91cd489e9178c27a7317c07cb49ddbab73
mohammedchakir <zerroukimedchakir@gmail.com>
2023-08-21 18:59:42 -0700

    changes

--- 6_get_precision.c
+++ 6_get_precision.c
@@ -1,12 +1,11 @@
 #include "main.h"
-#include <stdio.h>
-#include <stdarg.h>
 
 /**
- * get_precision - Calculates the precision for printing
+ * get_precision - Calculates the precision for printing.
  * @format: Formatted string in which to print the arguments
  * @i: List of arguments to be printed.
  * @list: list of arguments.
+ *
  * Return: Precision.
  */
 int get_precision(const char *format, int *i, va_list list)
--- main.h
+++ main.h
@@ -106,7 +106,6 @@ int flags, int width, int precision, int size);
 int is_printable(char);
 int append_hexa_code(char, char[], int);
 int is_digit(char);
-int main(void);
 
 long int convert_size_number(long int num, int size);
 long int convert_size_unsgnd(unsigned long int num, int size);
